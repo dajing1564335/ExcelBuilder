d5796729758e9e6bd3e318b07c663056f969c44f
dajing1564335 <549393807@qq.com>
2025-01-13 18:51:54 +0900

    Revert "1"
    This reverts commit 1059bce40f1adce6b71f403dfdc0a16899ce9204.

---
+++ Assets/Scripts/Editor/ExcelBuilder/ExcelBuilderSO.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ExcelBuilderSO : ScriptableObject
+{
+    public bool LoadMsgData;
+    public bool LoadTableData;
+    public List<string> EnumNames = new();
+    public List<string> WarningLogs = new();
+
+    public void Updata(List<string> names)
+    {
+        foreach (var name in EnumNames)
+        {
+            if (!names.Contains(name))
+            {
+                if (Directory.Exists(ExcelBuilder.TableFolder + name))
+                {
+                    Directory.Delete(ExcelBuilder.TableFolder + name, true);
+                }
+                File.Delete(ExcelBuilder.TableFolder + name + ".meta");
+                File.Delete(ExcelBuilder.DataFolder + name + "Data.asset");
+                File.Delete(ExcelBuilder.DataFolder + name + "Data.asset.meta");
+                File.Delete(ExcelBuilder.RefFolder + name + "Ref.asset");
+                File.Delete(ExcelBuilder.RefFolder + name + "Ref.asset.meta");
+            }
+        }
+        EnumNames = names;
+    }
+}
--- Assets/Scripts/Editor/UIMenuItem.cs
+++ Assets/Scripts/Editor/UIMenuItem.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 public class UIMenuItem
 {
     //const string TextEx = "Assets/Prefabs/ExUI/TextEx.prefab";
-    //const string TextMeshProUGUIEx = "Assets/Prefabs/ExUI/TextEx (TMP).prefab";
+    const string TextMeshProUGUIEx = "Assets/Prefabs/ExUI/TextEx (TMP).prefab";
     //const string ButtonEx = "Assets/Prefabs/ExUI/ButtonEx.prefab";
-    //const string ButtonExTMP = "Assets/Prefabs/ExUI/ButtonEx (TMP).prefab";
+    const string ButtonExTMP = "Assets/Prefabs/ExUI/ButtonEx (TMP).prefab";
     //const string DropdownEx = "Assets/Prefabs/ExUI/DropdownEx.prefab";
-    //const string DropdownExTMP = "Assets/Prefabs/ExUI/DropdownEx (TMP).prefab";
-    
-    const string TextMeshProUGUIExGUID = "985ce8671b4188341927b12e48b06319";
-    const string ButtonExTMPGUID = "d765ce726e18c3f4781d833f714d40fe";
-    const string DropdownExTMPGUID = "faa4720bafefeeb40b7fc62f6d9c6731";
+    const string DropdownExTMP = "Assets/Prefabs/ExUI/DropdownEx (TMP).prefab";
 
     //[MenuItem("GameObject/UI/Text - Ex", false, 2001)]
     //static void CreateTextEx()
@@ -26,8 +22,7 @@ public class UIMenuItem
     [MenuItem("GameObject/UI/Text - TextMeshPro - Ex", false, 2001)]
     static void CreateTextMeshProUGUIEx()
     {
-        //GameObject obj = Object.Instantiate((GameObject)AssetDatabase.LoadMainAssetAtPath(TextMeshProUGUIEx), Selection.activeTransform);
-        GameObject obj = Object.Instantiate((GameObject)AssetDatabase.LoadMainAssetAtPath(AssetDatabase.GUIDToAssetPath(TextMeshProUGUIExGUID)), Selection.activeTransform);
+        GameObject obj = Object.Instantiate((GameObject)AssetDatabase.LoadMainAssetAtPath(TextMeshProUGUIEx), Selection.activeTransform);
         obj.name = "TextEx (TMP)";
         obj.transform.localPosition = Vector3.zero;
         Selection.activeGameObject = obj;
@@ -45,8 +40,7 @@ public class UIMenuItem
     [MenuItem("GameObject/UI/Button - TextMeshPro - Ex", false, 2031)]
     static void CreateButtonExTMP()
     {
-        //GameObject obj = Object.Instantiate((GameObject)AssetDatabase.LoadMainAssetAtPath(ButtonExTMP), Selection.activeTransform);
-        GameObject obj = Object.Instantiate((GameObject)AssetDatabase.LoadMainAssetAtPath(AssetDatabase.GUIDToAssetPath(ButtonExTMPGUID)), Selection.activeTransform);
+        GameObject obj = Object.Instantiate((GameObject)AssetDatabase.LoadMainAssetAtPath(ButtonExTMP), Selection.activeTransform);
         obj.name = "ButtonEx";
         obj.transform.localPosition = Vector3.zero;
         Selection.activeGameObject = obj;
@@ -64,8 +58,7 @@ public class UIMenuItem
     [MenuItem("GameObject/UI/Dropdown - TextMeshPro - Ex", false, 2036)]
     static void CreateDropdownExTMP()
     {
-        //GameObject obj = Object.Instantiate((GameObject)AssetDatabase.LoadMainAssetAtPath(DropdownExTMP), Selection.activeTransform);
-        GameObject obj = Object.Instantiate((GameObject)AssetDatabase.LoadMainAssetAtPath(AssetDatabase.GUIDToAssetPath(DropdownExTMPGUID)), Selection.activeTransform);
+        GameObject obj = Object.Instantiate((GameObject)AssetDatabase.LoadMainAssetAtPath(DropdownExTMP), Selection.activeTransform);
         obj.name = "DropdownEx";
         obj.transform.localPosition = Vector3.zero;
         Selection.activeGameObject = obj;
