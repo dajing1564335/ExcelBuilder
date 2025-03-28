4f3b4b8d3c974e1ee96615adeaca5f85a0f5681b
dajing1564335 <549393807@qq.com>
2025-03-28 20:59:09 +0900

    1

--- Assets/ExcelDB/Editor/ExUI/UIMenuItem.cs
+++ Assets/ExcelDB/Editor/ExUI/UIMenuItem.cs
@@ -1,69 +1,66 @@
 using UnityEditor;
 using UnityEngine;
 
-namespace Table
+public class UIMenuItem
 {
-    public class UIMenuItem
+    //const string TextEx = "Assets/ExcelDB/Runtime/ExUI/TextEx.prefab";
+    const string TextMeshProUGUIEx = "Assets/ExcelDB/Runtime/ExUI/TextEx (TMP).prefab";
+    //const string ButtonEx = "Assets/ExcelDB/Runtime/ExUI/ButtonEx.prefab";
+    const string ButtonExTMP = "Assets/ExcelDB/Runtime/ExUI/ButtonEx (TMP).prefab";
+    //const string DropdownEx = "Assets/ExcelDB/Runtime/ExUI/DropdownEx.prefab";
+    const string DropdownExTMP = "Assets/ExcelDB/Runtime/ExUI/DropdownEx (TMP).prefab";
+
+    //[MenuItem("GameObject/UI/Text - Ex", false, 2001)]
+    //static void CreateTextEx()
+    //{
+    //    GameObject obj = Object.Instantiate((GameObject)AssetDatabase.LoadMainAssetAtPath(TextEx), Selection.activeTransform);
+    //    obj.name = "TextEx";
+    //    obj.transform.localPosition = Vector3.zero;
+    //    Selection.activeGameObject = obj;
+    //}
+
+    [MenuItem("GameObject/UI/Text - TextMeshPro - Ex", false, 2001)]
+    static void CreateTextMeshProUGUIEx()
     {
-        //const string TextEx = "Assets/ExcelDB/Runtime/ExUI/TextEx.prefab";
-        const string TextMeshProUGUIEx = "Assets/ExcelDB/Runtime/ExUI/TextEx (TMP).prefab";
-        //const string ButtonEx = "Assets/ExcelDB/Runtime/ExUI/ButtonEx.prefab";
-        const string ButtonExTMP = "Assets/ExcelDB/Runtime/ExUI/ButtonEx (TMP).prefab";
-        //const string DropdownEx = "Assets/ExcelDB/Runtime/ExUI/DropdownEx.prefab";
-        const string DropdownExTMP = "Assets/ExcelDB/Runtime/ExUI/DropdownEx (TMP).prefab";
+        GameObject obj = Object.Instantiate((GameObject)AssetDatabase.LoadMainAssetAtPath(TextMeshProUGUIEx), Selection.activeTransform);
+        obj.name = "TextEx (TMP)";
+        obj.transform.localPosition = Vector3.zero;
+        Selection.activeGameObject = obj;
+    }
 
-        //[MenuItem("GameObject/UI/Text - Ex", false, 2001)]
-        //static void CreateTextEx()
-        //{
-        //    GameObject obj = Object.Instantiate((GameObject)AssetDatabase.LoadMainAssetAtPath(TextEx), Selection.activeTransform);
-        //    obj.name = "TextEx";
-        //    obj.transform.localPosition = Vector3.zero;
-        //    Selection.activeGameObject = obj;
-        //}
+    //[MenuItem("GameObject/UI/Button - Ex", false, 2031)]
+    //static void CreateButtonEx()
+    //{
+    //    GameObject obj = Object.Instantiate((GameObject)AssetDatabase.LoadMainAssetAtPath(ButtonEx), Selection.activeTransform);
+    //    obj.name = "ButtonEx";
+    //    obj.transform.localPosition = Vector3.zero;
+    //    Selection.activeGameObject = obj;
+    //}
 
-        [MenuItem("GameObject/UI/Text - TextMeshPro - Ex", false, 2001)]
-        static void CreateTextMeshProUGUIEx()
-        {
-            GameObject obj = Object.Instantiate((GameObject)AssetDatabase.LoadMainAssetAtPath(TextMeshProUGUIEx), Selection.activeTransform);
-            obj.name = "TextEx (TMP)";
-            obj.transform.localPosition = Vector3.zero;
-            Selection.activeGameObject = obj;
-        }
+    [MenuItem("GameObject/UI/Button - TextMeshPro - Ex", false, 2031)]
+    static void CreateButtonExTMP()
+    {
+        GameObject obj = Object.Instantiate((GameObject)AssetDatabase.LoadMainAssetAtPath(ButtonExTMP), Selection.activeTransform);
+        obj.name = "ButtonEx";
+        obj.transform.localPosition = Vector3.zero;
+        Selection.activeGameObject = obj;
+    }
 
-        //[MenuItem("GameObject/UI/Button - Ex", false, 2031)]
-        //static void CreateButtonEx()
-        //{
-        //    GameObject obj = Object.Instantiate((GameObject)AssetDatabase.LoadMainAssetAtPath(ButtonEx), Selection.activeTransform);
-        //    obj.name = "ButtonEx";
-        //    obj.transform.localPosition = Vector3.zero;
-        //    Selection.activeGameObject = obj;
-        //}
+    //[MenuItem("GameObject/UI/Dropdown - Ex", false, 2036)]
+    //static void CreateDropdownEx()
+    //{
+    //    GameObject obj = Object.Instantiate((GameObject)AssetDatabase.LoadMainAssetAtPath(DropdownEx), Selection.activeTransform);
+    //    obj.name = "DropdownEx";
+    //    obj.transform.localPosition = Vector3.zero;
+    //    Selection.activeGameObject = obj;
+    //}
 
-        [MenuItem("GameObject/UI/Button - TextMeshPro - Ex", false, 2031)]
-        static void CreateButtonExTMP()
-        {
-            GameObject obj = Object.Instantiate((GameObject)AssetDatabase.LoadMainAssetAtPath(ButtonExTMP), Selection.activeTransform);
-            obj.name = "ButtonEx";
-            obj.transform.localPosition = Vector3.zero;
-            Selection.activeGameObject = obj;
-        }
-
-        //[MenuItem("GameObject/UI/Dropdown - Ex", false, 2036)]
-        //static void CreateDropdownEx()
-        //{
-        //    GameObject obj = Object.Instantiate((GameObject)AssetDatabase.LoadMainAssetAtPath(DropdownEx), Selection.activeTransform);
-        //    obj.name = "DropdownEx";
-        //    obj.transform.localPosition = Vector3.zero;
-        //    Selection.activeGameObject = obj;
-        //}
-
-        [MenuItem("GameObject/UI/Dropdown - TextMeshPro - Ex", false, 2036)]
-        static void CreateDropdownExTMP()
-        {
-            GameObject obj = Object.Instantiate((GameObject)AssetDatabase.LoadMainAssetAtPath(DropdownExTMP), Selection.activeTransform);
-            obj.name = "DropdownEx";
-            obj.transform.localPosition = Vector3.zero;
-            Selection.activeGameObject = obj;
-        }
+    [MenuItem("GameObject/UI/Dropdown - TextMeshPro - Ex", false, 2036)]
+    static void CreateDropdownExTMP()
+    {
+        GameObject obj = Object.Instantiate((GameObject)AssetDatabase.LoadMainAssetAtPath(DropdownExTMP), Selection.activeTransform);
+        obj.name = "DropdownEx";
+        obj.transform.localPosition = Vector3.zero;
+        Selection.activeGameObject = obj;
     }
 }
